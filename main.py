8890fe8ef3652419363e4a51cbf464b227c1c6f4
Abhijith Sudhakaran <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2022-05-23 19:51:15 +0530

    Create main.py
    Created 😹

---
+++ main.py
@@ -0,0 +1,22 @@
+# Copyright 2022 - https://github.com/Abhijith-Sudhakaran . 
+# Don't Remove the credit line huh😐
+
+import asyncio
+from pyrogram import Client, idle
+from config import API_ID, API_HASH, BOT_TOKEN
+from player.videoplayer import app
+from player.videoplayer import call_py
+bot = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="player"),
+)
+
+
+bot.start()
+app.start()
+call_py.start()
+idle()
+    
