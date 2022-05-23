6213cbd9e3b2601cc0c6378c6eb08c299b44c215
Abhijith Sudhakaran <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2022-05-23 20:44:35 +0530

    Create tr.py
    Created 😹

---
+++ plugins/tr.py
@@ -0,0 +1,13 @@
+import os
+
+HELP_TEXT =f"**SETTINGS** \n\n`Click on the buttons below to get documentation about specific modules..`"
+START_TEXT = '👋 `Hey My Name Is` MusicFlix Bot 👻 \n\n`Lets Enjoy Cinematic View  of Group Video Player With Your Friends ❤️😊`'
+STREAM_TEXT = '**COMMANDS** \n\n`/stream` - Reply to Video or File That You Want To stream In Vc. \n\n`/stream` - use [yt link] \n\n`/play` - reply to any audio file or utube url.. \n\n`/endstream` - Stop the stream'
+INFO_TEXT =f"**COMMANDS** \n\nGet information about something!\n\n`/info` - reply to any user!\n\n`/whois` - get the information of an user!"
+PASTE_TEXT = '**COMMANDS** \n\nPaste some texts or documents on pasting website!\n\n`/paste` - Reply To Any File!'
+VSONG_TEXT = '**COMMANDS** \n\nDo You Want Video Song!\n\n`/video` - **[Song Name]** Search Your Favourite Song!\n\n`/vsong` - **[Song Name]** Try!'
+TGPK_TEXT =f"**COMMANDS** \n\nDo as you wish with telegra.ph module!\n\n`/telegraph` - Reply to a supported media file!\n\n`/tgm` - Reply to a any msg or file."
+
+
+# എന്താടാ മോനെ അടിച്ചു അടിച്ചു മാറ്റാൻ വന്നതാണോ? നാണം വേണം കേട്ടോ കുറച്ച് 😜😹.
+# എന്തായാലും എടുത്തു bug ആരികും മുഴുവൻ നോക്കി ഒക്കെ add ആക്ക് കേട്ടോ 🤭
