3260d2c60cdbfa76dfcd1ec049b4a5704dc6ec87
Abhijith Sudhakaran <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2022-05-29 11:12:46 +0530

    Update inline.py
    Known Bug fixed 🐞

--- player/inline.py
+++ player/inline.py
@@ -1,9 +1,9 @@
+# Inline PyFile Version- 1.5.0 • 
+# Last Updated on 29 May 2022
+
 from pyrogram import Client, errors
-from pyrogram.types import (
-    InlineQuery,
-    InlineQueryResultArticle,
-    InputTextMessageContent,
-)
+from pyrogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
+
 from youtubesearchpython import VideosSearch
 
 
@@ -16,9 +16,9 @@ async def inline(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="type a youtube video name...",
+            switch_pm_text="Type a YouTube video name...",
             switch_pm_parameter="help",
-            cache_time=0,
+            cache_time=0
         )
     else:
         search = VideosSearch(search_query, limit=50)
@@ -28,21 +28,27 @@ async def inline(client: Client, query: InlineQuery):
                 InlineQueryResultArticle(
                     title=result["title"],
                     description="{}, {} views.".format(
-                        result["duration"], result["viewCount"]["short"]
+                        result["duration"],
+                        result["viewCount"]["short"]
                     ),
                     input_message_content=InputTextMessageContent(
-                        "🔗 https://www.youtube.com/watch?v={}".format(result["id"])
+                        "https://www.youtube.com/watch?v={}".format(
+                            result["id"]
+                        )
                     ),
-                    thumb_url=result["thumbnails"][0]["url"],
+                    thumb_url=result["thumbnails"][0]["url"]
                 )
             )
 
         try:
-            await query.answer(results=answers, cache_time=0)
+            await query.answer(
+                results=answers,
+                cache_time=0
+            )
         except errors.QueryIdInvalid:
             await query.answer(
                 results=answers,
                 cache_time=0,
-                switch_pm_text="Something went Error: search timed out",
+                switch_pm_text="Error: Search timed out",
                 switch_pm_parameter="",
             )
